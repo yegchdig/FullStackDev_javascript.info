053dc8ae3df2e7aa844ccc484f82ecabc469b8dc
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-20 11:49:14 +0100

    restored all folders and files

---
+++ 02-array-methods/task_7_Map-to-objects_FIN/script.js
@@ -0,0 +1,23 @@
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let usersMapped = users.map(function(item) {
+    item.fullName = item.name + " " + item.surname;
+    delete item.name;
+    delete item.surname;
+    return item;
+});
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+
+console.log( usersMapped[0].id ) // 1
+console.log( usersMapped[0].fullName ) // Вася Пупкин
