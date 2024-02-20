f20e68edb6d42b6c9d534b1a8c94a2126358e0a4
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-20 10:04:09 +0100

    wrote the code and solved the task; my solution is different from the author's answer

--- 02-array-methods/task_7_Map-to-objects/script.js
+++ 02-array-methods/task_7_Map-to-objects/script.js
@@ -1,24 +1,23 @@
-function Calculator() {
-    this.calculate = function(str) {
-        this.a = str.slice(0, str.indexOf(' '));
-        this.operator = str.slice(str.indexOf(' ') + 1, str.lastIndexOf(' '));
-        this.b = str.slice(str.lastIndexOf(' ') + 1);
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
-        switch (this.operator) {
-            case '+':
-                res = parseFloat(this.a) + parseFloat(this.b);
-                break;
-            case '-':
-                res = parseFloat(this.a) - parseFloat(this.b);
-                break;
-            default:
-                res = "There is no such action."
-                break;
-            }
-            
-        return res;
-    }
-}
+let users = [ vasya, petya, masha ];
 
-let calc = new Calculator;
-console.log( calc.calculate("3 + 7") ); // 10
+let usersMapped = users.map(function(item) {
+    item.fullName = item.name + " " + item.surname;
+    delete item.name;
+    delete item.surname;
+    return item;
+});
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+
+console.log( usersMapped[0].id ) // 1
+console.log( usersMapped[0].fullName ) // Вася Пупкин
