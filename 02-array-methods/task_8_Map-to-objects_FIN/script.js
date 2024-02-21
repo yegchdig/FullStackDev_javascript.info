3e11a110ce1ee87ca73ae655620acc00929623e3
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-21 12:21:41 +0100

    hid the description in russian in index.html; added english to script.js

--- 02-array-methods/task_8_Map-to-objects_FIN/script.js
+++ 02-array-methods/task_8_Map-to-objects_FIN/script.js
@@ -1,8 +1,10 @@
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
+//------------------------- ENGLISH
 
-let users = [ vasya, petya, masha ];
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
 
 let usersMapped = users.map(function(item) {
     item.fullName = item.name + " " + item.surname;
@@ -13,11 +15,39 @@ let usersMapped = users.map(function(item) {
 
 /*
 usersMapped = [
-  { fullName: "Вася Пупкин", id: 1 },
-  { fullName: "Петя Иванов", id: 2 },
-  { fullName: "Маша Петрова", id: 3 }
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
 ]
 */
 
 console.log( usersMapped[0].id ) // 1
-console.log( usersMapped[0].fullName ) // Вася Пупкин
+console.log( usersMapped[0].fullName ) // John Smith
+
+
+
+//------------------------- RUSSIAN
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map(function(item) {
+//     item.fullName = item.name + " " + item.surname;
+//     delete item.name;
+//     delete item.surname;
+//     return item;
+// });
+
+// /*
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 }
+// ]
+// */
+
+// console.log( usersMapped[0].id ) // 1
+// console.log( usersMapped[0].fullName ) // Вася Пупкин
