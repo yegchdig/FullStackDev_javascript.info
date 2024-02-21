1a4d64ae00975baa2b74e667edf8cce6caa31277
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-21 11:57:29 +0100

    solved the task

---
+++ 02-array-methods/task_8_Sort-users-by-age/script.js
@@ -0,0 +1,22 @@
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+function sortByAge(arr) {
+  function compare(a, b) {
+    if (a.age > b.age) return 1;
+    if (a.age == b.age) return 0;
+    if (a.age < b.age) return -1;
+  }
+
+  arr.sort(compare)
+}
+
+sortByAge(arr);
+
+// теперь: [vasya, masha, petya]
+console.log(arr[0].name); // Вася
+console.log(arr[1].name); // Маша
+console.log(arr[2].name); // Петя
