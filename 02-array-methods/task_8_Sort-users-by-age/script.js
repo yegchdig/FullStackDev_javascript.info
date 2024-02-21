185dd55aef8726bf443b779366831a4793064b13
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-21 12:03:36 +0100

    added english language

--- 02-array-methods/task_8_Sort-users-by-age/script.js
+++ 02-array-methods/task_8_Sort-users-by-age/script.js
@@ -1,8 +1,10 @@
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+//------------------------- ENGLISH
 
-let arr = [ vasya, petya, masha ];
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
 
 function sortByAge(arr) {
   function compare(a, b) {
@@ -16,7 +18,34 @@ function sortByAge(arr) {
 
 sortByAge(arr);
 
-// теперь: [vasya, masha, petya]
-console.log(arr[0].name); // Вася
-console.log(arr[1].name); // Маша
-console.log(arr[2].name); // Петя
+// now: [john, mary, pete]
+console.log(arr[0].name); // John
+console.log(arr[1].name); // Mary
+console.log(arr[2].name); // Pete
+
+
+
+//------------------------- RUSSIAN
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// function sortByAge(arr) {
+//   function compare(a, b) {
+//     if (a.age > b.age) return 1;
+//     if (a.age == b.age) return 0;
+//     if (a.age < b.age) return -1;
+//   }
+
+//   arr.sort(compare)
+// }
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// console.log(arr[0].name); // Вася
+// console.log(arr[1].name); // Маша
+// console.log(arr[2].name); // Петя
