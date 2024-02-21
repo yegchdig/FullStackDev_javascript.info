cc2b7bd53bec39af60fd9a6ede74e05de0e5ac48
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-21 15:51:16 +0100

    changed the script.js file

--- 02-array-methods/task_10_Shuffle-an-array/script.js
+++ 02-array-methods/task_10_Shuffle-an-array/script.js
@@ -1,51 +1,15 @@
-//------------------------- ENGLISH
-
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
-
-let arr = [ pete, john, mary ];
-
-function sortByAge(arr) {
-  function compare(a, b) {
-    if (a.age > b.age) return 1;
-    if (a.age == b.age) return 0;
-    if (a.age < b.age) return -1;
-  }
-
-  arr.sort(compare)
+function shuffle(array) {
+  
 }
 
-sortByAge(arr);
+let arr = [1, 2, 3];
 
-// now: [john, mary, pete]
-console.log(arr[0].name); // John
-console.log(arr[1].name); // Mary
-console.log(arr[2].name); // Pete
+shuffle(arr);
+// arr = [3, 2, 1]
 
+shuffle(arr);
+// arr = [2, 1, 3]
 
-
-//------------------------- RUSSIAN
-
-// let vasya = { name: "Вася", age: 25 };
-// let petya = { name: "Петя", age: 30 };
-// let masha = { name: "Маша", age: 28 };
-
-// let arr = [ vasya, petya, masha ];
-
-// function sortByAge(arr) {
-//   function compare(a, b) {
-//     if (a.age > b.age) return 1;
-//     if (a.age == b.age) return 0;
-//     if (a.age < b.age) return -1;
-//   }
-
-//   arr.sort(compare)
-// }
-
-// sortByAge(arr);
-
-// // теперь: [vasya, masha, petya]
-// console.log(arr[0].name); // Вася
-// console.log(arr[1].name); // Маша
-// console.log(arr[2].name); // Петя
+shuffle(arr);
+// arr = [3, 1, 2]
+// ...
