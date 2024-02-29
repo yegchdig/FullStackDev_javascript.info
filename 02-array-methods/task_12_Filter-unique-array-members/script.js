34986e354260584baaf60898f59499c8430e411f
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-29 10:47:42 +0100

    added description in english

---
+++ 02-array-methods/task_12_Filter-unique-array-members/script.js
@@ -0,0 +1,7 @@
+function unique(arr) {
+  /* your code */
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+alert( unique(strings) ); // Hare, Krishna, :-O
