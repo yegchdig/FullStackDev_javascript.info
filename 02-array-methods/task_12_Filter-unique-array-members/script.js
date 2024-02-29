af62b8163db9eb51fa606b945cddea98faadcbca
yegchdig <yegor.chebakov.digital@gmail.com>
2024-02-29 11:32:23 +0100

    solved using the indexOf array method

--- 02-array-methods/task_12_Filter-unique-array-members/script.js
+++ 02-array-methods/task_12_Filter-unique-array-members/script.js
@@ -1,11 +1,33 @@
+// function unique(arr) {
+//     let arrNew = [];
+
+//     for (const item of arr) {
+//         if (!arrNew.includes(item)) {
+//             arrNew.push(item);
+//         }
+//     };
+//     return arrNew;
+// }
+
+// let strings = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+// console.log(unique(strings)); // Hare, Krishna, :-O
+
+
+
+// -----------------------------------------------------------
+
+
+
 function unique(arr) {
     let arrNew = [];
 
     for (const item of arr) {
+        let res = arr.indexOf(item);
         if (!arrNew.includes(item)) {
-            arrNew.push(item);
+            arrNew.push(arr[res]);
         }
-    };
+    }
     return arrNew;
 }
 
